7b6e0a0d0cc2387439e95c1b412480d3f6139247
aaescobar <47513774+aaescobar@users.noreply.github.com>
2022-04-11 11:48:47 -0400

    Everything

---
+++ CriminalReportingTool/src/main/java/com/example/demo/CriminalReportingToolApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalReportingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriminalReportingToolApplication.class, args);
+		System.out.println("Bienvenido A Reporte Criminal");
+	}
+
+}
